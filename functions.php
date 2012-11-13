b088a2191fd843d3ce2a9d465a2244577c9e28ce
Guilherme Guerra <guerrinha@comum.org>
2012-11-13 17:23:26 -0200

    adicioando novas areas de widgets para a paǵina de resultados

--- functions.php
+++ functions.php
@@ -13,6 +13,26 @@ function twentyeleven_procergs_widgets_init() {
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	) );
+
+	register_sidebar( array(
+		'name' => __( 'Resultados 2012', 'twentyeleven' ),
+		'id' => 'sidebar-7',
+		'description' => __( 'An optional widget area for your site footer', 'twentyeleven' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name' => __( 'Resultados 2011', 'twentyeleven' ),
+		'id' => 'sidebar-8',
+		'description' => __( 'An optional widget area for your site footer', 'twentyeleven' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
 }
 add_action( 'widgets_init', 'twentyeleven_procergs_widgets_init' );
 
